87f5bff9780e81f3139c22d7324222acd82d8418
pdostal <git@pdostal.cz>
2016-01-24 09:31:04 +0100

    Create simple ruby script for parsing /8 prefixes

---
+++ assignments.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+# ./assignments.rb iana.tmp RIPE\ NCC yaml
+
+File.open( "./#{ARGV[0]}", 'r' ).each do |line|
+  if line =~ /^[0-9]+\/[0-9]+,#{ARGV[1]}/
+    line = line.split','
+    line[0] = line[0].split'/'
+
+    if !ARGV[2].nil?
+      type = 'yaml' if ARGV[2].strip.downcase == "yaml"
+    else
+      type = 'other'
+    end
+
+    if type == 'yaml'
+      puts '- ' + line[0][0] + '.0.0.0/' + line[0][1]
+    else
+      puts line[0][0] + '.0.0.0/' + line[0][1]
+    end
+  end
+end
