0490372b1ae961fccb06004a130367fec7865a87
pdostal <git@pdostal.cz>
2016-01-24 11:03:32 +0100

    Implement comments for assignments.rb & ranges.rb

--- assignments.rb
+++ assignments.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-# ./assignments.rb iana.tmp RIPE\ NCC yaml
+# ./assignments.rb iana.tmp RIPE\ NCC yaml RIPE
 
 File.open( "./#{ARGV[0]}", 'r' ).each do |line|
   if line =~ /^[0-9]+\/[0-9]+,#{ARGV[1]}/
@@ -14,7 +14,7 @@ File.open( "./#{ARGV[0]}", 'r' ).each do |line|
     end
 
     if type == 'yaml'
-      puts "- '" + line[0][0] + '.0.0.0/' + line[0][1] + " via \"eth0\"'"
+      puts "- '" + line[0][0] + '.0.0.0/' + line[0][1] + " via \"eth0\"' # #{ARGV[3]}"
     else
       puts line[0][0] + '.0.0.0/' + line[0][1]
     end
--- ranges.rb
+++ ranges.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-# ./ranges.rb ripe.tmp CZ\|SK yaml
+# ./ranges.rb ripe.tmp CZ\|SK yaml CZSK
 
 File.open( "./#{ARGV[0]}", 'r' ).each do |line|
   if line =~ /^[a-zA-Z]+\|(#{ARGV[1]})\|ipv4/
@@ -15,7 +15,7 @@ File.open( "./#{ARGV[0]}", 'r' ).each do |line|
     line[4] = 32 - Math.log2(line[4].to_f).floor
 
     if type == 'yaml'
-      puts "- '" + line[3] + '/' + line[4].to_s + " via \"eth0\"'"
+      puts "- '" + line[3] + '/' + line[4].to_s + " via \"eth0\"' # #{ARGV[3]}"
     else
       puts line[3] + '/' + line[4].to_s
     end
